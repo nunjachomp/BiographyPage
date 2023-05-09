9966ba621b2d64c66733d0235011928464cac620
nunjachomp <nunjachomp@gmail.com>
2023-05-09 16:03:39 +0300

    Site Visual Updates

--- SalantPrado_Christian_ITCPrecourse/js/about.js
+++ SalantPrado_Christian_ITCPrecourse/js/about.js
@@ -1,12 +1,12 @@
 const prevButton = document.querySelector('.prev');
 const nextButton = document.querySelector('.next');
-const amoreGalleryImgs = document.querySelectorAll (".gallery-img");
+const cityMaps = document.querySelectorAll (".gallery-img");
 let currentlySelected = 0;
 
 prevButton.addEventListener('click', function() {
-   amoreGalleryImgs[currentlySelected].classList.remove("active");
+   cityMaps[currentlySelected].classList.remove("active");
    currentlySelected--;
-   amoreGalleryImgs[currentlySelected].classList.add("active");
+   cityMaps[currentlySelected].classList.add("active");
    nextButton.disabled = false;
 
    if (currentlySelected === 0) {
@@ -15,12 +15,12 @@ prevButton.addEventListener('click', function() {
 })
 
 nextButton.addEventListener('click', function() {
-    amoreGalleryImgs[currentlySelected].classList.remove("active");
+    cityMaps[currentlySelected].classList.remove("active");
     currentlySelected++; //currentlySelected = currentlySelected + 1
-    amoreGalleryImgs[currentlySelected].classList.add("active");
+    cityMaps[currentlySelected].classList.add("active");
     prevButton.disabled = false;
 
-    if (amoreGalleryImgs.length === currentlySelected + 1) {
+    if (cityMaps.length === currentlySelected + 1) {
         nextButton.disabled = true;
     }
 })
