67cca7699466a55fdaa6e774e45696796b9161d6
nunjachomp <nunjachomp@gmail.com>
2023-05-11 17:17:56 +0300

    New Rep

--- SalantPrado_Christian_ITCPrecourse/js/contact.js
+++
@@ -1,7 +0,0 @@
-contactForm.addEventListener('input', () => {
-    if (firstname.value.length > 0 && email.value.length > 0) {
-        submitButton.removeAttribute('disabled');
-    } else {
-        submitButton.setAttribute('disabled', 'disabled');
-    }
-});
